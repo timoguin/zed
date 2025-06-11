13dea950862280eb3982d9058b8c785a5650026f
Ben Kunkle <ben@zed.dev>
2025-06-11 12:42:48 +0200

    add todo

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -1405,6 +1405,7 @@ fn reload_keymaps(cx: &mut App, user_key_bindings: Vec<KeyBinding>) {
         "New Window",
         workspace::NewWindow,
     )]);
+    // todo! nicer api here
     settings_ui::keybindings::KeymapEventChannel::trigger_keymap_changed(cx);
 }
 
